fd9cfdffe1c9d4877d0be98ef24d6bad16e45dac
Fadi Nammour <github@nammour.com>
2019-07-28 18:12:35 +0200

    resume

--- resume/script/company.js
+++ resume/script/company.js
@@ -6,12 +6,6 @@ var company = {
     library: "",
     OnPageLoad: () => {
         return $.when(new User().code(sr.$_REQUEST("code"), "=").findAll()).then(ret => {
-            $.each(ret, (_, r) => {
-                r._user_User_Educations.sort((a, b) => b._from - a._from);
-                r._user_User_Experiences.sort((a, b) => b._from - a._from);
-                r._user_User_Skills.sort((a, b) => b._percentage - a._percentage);
-                r._user_User_Publications.sort((a, b) => b._date - a._date);
-            });
             window.me = ret[0];
             window.frames[0].location = 'website/' + window.me._template._code;
         });
